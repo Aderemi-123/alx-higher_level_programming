aaf2c1345eed6db499afb785a0c440c178141b7d
Ogunbowale Yusuf Aderemi <ogunbowaleyusufaderemi4@gmail.com>
2023-03-15 09:04:35 -0700

    4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,2 +1,18 @@
 #!/usr/bin/node
-console.log(process.argv[2] + ' is ' + process.argv[3]);
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    [this.width, this.height] = [this.width * 2, this.height * 2];
+  }
+};
